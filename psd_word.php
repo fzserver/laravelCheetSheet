498ef71e808072a0f5f9f2144800e79711addf90
Abhijeet Kumar <85831655+aryaabhii@users.noreply.github.com>
2024-02-28 10:06:43 +0530

    Create psd_word.php

---
+++ psd_word.php
@@ -0,0 +1,14 @@
+// abhijeet's code start
+        if ($request->status == 1) {
+            return $pdf->download('my.pdf'); // it will download the pdf file.
+        } elseif ($request->status == 2) {
+            // it will help to download word
+            $headers = [
+                'content-type'        => 'text/html',
+                'Content-Disposition' => 'attatchement;Filename=my.doc',
+            ];
+  
+            return \Response::make(view('view_name', compact('data', 'request')), 200, $headers);
+        } else {
+            return redirect()->back()->with('success', 'Sorry! Not available.');
+        }
